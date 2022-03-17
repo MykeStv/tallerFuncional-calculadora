c87a13ba08595ef81a4c7c14c5055f7d07ce04fb
MykeStv <mykestv22@gmail.com>
2022-03-17 10:26:38 -0500

    Creacion del proyecto

---
+++ calculadora.js
@@ -0,0 +1,19 @@
+//Maycol Steven Rincon Bayona
+//Taller de programación funcional
+
+const sum = (num1, num2) => {
+    return num1 + num2
+}
+
+const diff = (num1, num2) => {
+    return num1 - num2
+}
+
+const product = (num1, num2) => {
+    if (num2 == 1) {
+        
+    }
+}
+
+// console.log(suma(3,5))
+// console.log(diff(3,5))
